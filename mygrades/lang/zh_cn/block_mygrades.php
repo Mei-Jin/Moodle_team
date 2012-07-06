bca24ee0c5c190e5b0b5faf53a209df76ac02dd5
MJ <mei.jin@ucdconnect.ie>
2012-07-06 19:27:00 +0100

    Upload in 6th July   lastest version of my grades

---
+++ mygrades/lang/zh_cn/block_mygrades.php
@@ -0,0 +1,38 @@
+﻿<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_my_courses', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   block_my_courses
+ * @author    Dan Marsden <dan@danmarsden.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = '我的成绩 ';
+$string['categorysettings'] = '分类设置 ';
+$string['category'] = '分类 ';
+$string['display'] = '显示 ';
+$string['cascade'] = '层叠 ';
+$string['enrolled'] = '注册 ';
+$string['detail'] = '详细内容 ';
+$string['full'] = '全部 ';
+$string['listing'] = '列表 ';
+$string['settingssaved'] = '保存分类设置 ';
+$string['modulename'] = 'modulename';
+$string['gradetblheader_course'] = '课程';
+$string['gradetblheader_grade'] = '成绩';
